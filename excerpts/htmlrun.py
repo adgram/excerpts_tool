f86d4a2665419d4650b78a717bb1317273a5e3dd
adgram <chcsyf@163.com>
2025-12-16 19:20:01 +0800

    修改了htmlgui相关bug，删除session文件

--- excerpts/htmlrun.py
+++ excerpts/htmlrun.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from pathlib import Path
 import logging
-from flask_session import Session
 
 from .htmlgui.views import app_bp, api_bp, config_bp, close_manager
 from .htmlgui import config
@@ -23,11 +22,9 @@ def create_flask_app():
         static_folder = static_path
     )
     app.config.from_object(config)
-    if app.config.get('SESSION_TYPE'):
-        Session(app)
     # 注册钩子：在请求结束后关闭线程安全的连接 (解决 g 存储对象的生命周期)
     app.teardown_appcontext(close_manager) 
-    # 注册蓝图 (app_bp 处理 / 路由, api_bp 处理 /api/* 路由)
+    # 注册蓝图
     app.register_blueprint(config_bp)
     app.register_blueprint(app_bp)
     app.register_blueprint(api_bp)
